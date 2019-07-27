9e1eed5547e1339ec825694d0153461953078a5e
xierang <1367572557@qq.com>
2019-07-27 18:48:12 +0800

    添加Git到本地仓库

---
+++ git_test/src/com/itheima/Git.java
@@ -0,0 +1,10 @@
+package com.itheima;
+
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("222222");
+        System.out.println("222222");
+        System.out.println("222222");
+        System.out.println("222222");
+    }
+}
